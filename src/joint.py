145ea626131e042990d96d45b36ced7a2363a3ad
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-12-18 10:10:44 +0100

    parsing done

--- src/joint.py
+++ src/joint.py
@@ -1,12 +1,13 @@
-#! /usr/env/bin pyhton2.7
+#! /usr/bin/env python2.7
 
 class Joint:
-    def __init__(self, name = "Unknown", offset = (0,0,0), rotation = (0,0,0),child = None, parent = None, isRoot = False):
+    def __init__(self, name = "Unknown", isRoot = False, offset = [0,0,0], rotation = [0,0,0], translation = [0,0,0], child = None, parent = None):
         self.name = name
         self.isRoot = isRoot
         self.offset = offset
         self.rotation = rotation
         self.child = child
+        self.translation = translation
         self.parent = parent
 
     def getRoot(self):
@@ -15,3 +16,20 @@ class Joint:
             parent = parent.parent
         print(self.name + " root is : " + parent.name)
         return parent
+    
+    def setOffset(self, offset):
+        self.offset = offset
+    
+    def setRotation(self, rotation):
+        self.rotation = rotation
+    
+    def addChild(self, child):
+        if(self.child == None):
+            self.child = []
+        self.child.append(child)
+    
+    def setParent(self, parent):
+        self.parent = parent
+    
+    def setTranslation(self, translation):
+        self.translation = translation
--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,88 @@
-#! /usr/env/bin pyhton2.7
+#! /usr/bin/env python3
 
-import joint
+from joint import Joint 
+import sys
+from tqdm import tqdm
+import time
 
-def parse(fileName = None):
-    if(fileName == None):
+def parse(argFile = None):
+    if(argFile == None):
         print("No file was given to parse")
         return
-    with open 
+    with open(argFile, 'r') as argument:
+        data = argument.read()
+        lines = data.split("\n")
+        currentJoint = Joint()
+        root = Joint()
+        compteur = 0;
+        for line in lines:
+            compteur += 1
+            if(line == "HIERARCHY"):
+                print("Parsing the hierarchical model : ")
+                continue
+            if("ROOT" in line):
+                jointName = line.split(' ')[1]
+                root = Joint(jointName, True)
+                currentJoint = root
+                continue
+            if("OFFSET" in line):
+                lineData = line.split(' ')
+                currentJoint.setOffset(lineData[1:])
+                continue
+            if("CHANNELS" in line):
+                lineData = line.split(' ')
+                if(lineData[1] == 6):
+                    currentJoint.setTranslation(lineData[2:5])
+                    currentJoint.setRotation(lineData[5:])
+                    continue
+                if(lineData[1] == 3):
+                    currentJoint.setTranslation(lineData[2:5])
+                    currentJoint.setRotation(lineData[5:])
+                    continue
+            if("JOINT" in line):
+                # print("ltoes" in currentJoint.name)
+                jointName = line.split(' ')[1]
+                joint = Joint(jointName, False)
+                if(currentJoint == None):
+                    print("Stooooooooooooop")
+                joint.setParent(currentJoint)
+                currentJoint.addChild(joint)
+                currentJoint = joint
+                continue
+            if("End" in line):
+                jointName = str(currentJoint.name + "-Child")
+                joint = Joint(jointName, False)
+                currentJoint.addChild(joint)
+                joint.setParent(currentJoint)
+                currentJoint = joint
+                continue
+            if("}" in line):
+                currentJoint = currentJoint.parent
+            if("MOTION" in line):
+                print("Arbre fini")
+                return root
+            
+
+def printH(joint):
+    if(joint.child == None):
+        print("go back !")
+        return
+    print(joint.name, end=' ')
+    print(" Has children : ")
+    liste = joint.child
+    for joints in liste:
+        print(joints.name, end=' ') 
+    print('\n')
+    for child in joint.child:
+        printH(child)
 
 
+
+def main():
+    root = parse(sys.argv[1])
+    # for i in tqdm(range(10)):
+    #     continue
+    #    time.sleep(0.1)
+    # printH(root)
+
+main()
